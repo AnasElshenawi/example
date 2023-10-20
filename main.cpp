62fcb8c1179c36c74661e773ad9fddcae35479c8
Anas Elshenawi <144563687+AnasElshenawi@users.noreply.github.com>
2023-10-20 19:25:50 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,16 +2,13 @@
 
 using namespace std;
 
-int main(){
-int x;
-int sum = 0;
-while(true){
-cin>>x;
-if(x > 0){
-    sum = sum+x;
-}else{
-break;
-}
-}
-cout<<sum;
+int main()
+{
+ int x = 2;
+ while(x <= 20){
+    if (x % 2 == 0 )
+          cout<<x<<endl;
+    x++;
+ }
+
 }
